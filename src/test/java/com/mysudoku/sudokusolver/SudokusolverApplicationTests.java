cde314f8552b6990edf8619831c86b205b4b11f7
jayana <jayana@pc-se-jayana.pc.symsoft.se>
2018-11-29 16:15:19 +0100

    Added mocks to the test

--- src/test/java/com/mysudoku/sudokusolver/SudokusolverApplicationTests.java
+++ src/test/java/com/mysudoku/sudokusolver/SudokusolverApplicationTests.java
@@ -1,10 +1,10 @@
 package com.mysudoku.sudokusolver;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.mysudoku.util.ConstraintValidator;
@@ -13,11 +13,12 @@ import com.mysudoku.util.ConstraintValidator;
 @SpringBootTest
 public class SudokusolverApplicationTests {
 
-  @Autowired
-  private SudukoSolver solver;
+ 
+  private SudukoSolver solver = mock(SudukoSolver.class);
+  private static ConstraintValidator validator = mock(ConstraintValidator.class);
 
   private static int[][] board;
-
+  
   private static final int BOARD_SIZE = 9;
 
   @Before
@@ -32,7 +33,6 @@ public class SudokusolverApplicationTests {
     thenValidateResults();
   }
 
-
   private void givenBoard() {
     board = new int[][] {{8, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 3, 6, 0, 0, 0, 0, 0},
         {0, 7, 0, 0, 9, 0, 2, 0, 0}, {0, 5, 0, 0, 0, 7, 0, 0, 0}, {0, 0, 0, 0, 4, 5, 7, 0, 0},
@@ -40,11 +40,11 @@ public class SudokusolverApplicationTests {
         {0, 9, 0, 0, 0, 0, 4, 0, 0}};
 
   }
-
+  
   private void thenValidateResults() {
-    assertTrue(ConstraintValidator.rowConstraint(board, BOARD_SIZE - 1));
-    assertTrue(ConstraintValidator.columnConstraint(board, BOARD_SIZE - 1));
-    assertTrue(ConstraintValidator.subsectionConstraint(board, BOARD_SIZE - 1, BOARD_SIZE - 1));
+    assertTrue(validator.rowConstraint(board, BOARD_SIZE - 1));
+    assertTrue(validator.columnConstraint(board, BOARD_SIZE - 1));
+    assertTrue(validator.subsectionConstraint(board, BOARD_SIZE - 1, BOARD_SIZE - 1));
   }
 
 }
